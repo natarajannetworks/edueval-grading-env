ab2847c8fef149740ae16dbf12da80cd13ca3e7c
Natarajan-Networks <128181030@sastra.ac.in>
2026-04-11 20:20:53 +0530

    Remove test file

--- test_env.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, 'src')
-from envs.grading_env.server.environment import GradingEnvironment
-env = GradingEnvironment(task_id=1)
-obs = env.reset()
-print('concept_coverage:', obs.concept_coverage)
-print('question:', obs.question_text)
-print('summary:', obs.answer_summary)
